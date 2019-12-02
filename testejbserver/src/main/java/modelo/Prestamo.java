b05423caa789eff0f553eca7b873311c6b253530
danie <danie@172.16.212.92>
2019-12-02 16:26:33 -0500

    Mensaje para la tabla

--- testejbserver/src/main/java/modelo/Prestamo.java
+++ testejbserver/src/main/java/modelo/Prestamo.java
@@ -16,58 +16,68 @@ import javax.persistence.TemporalType;
 @Entity
 public class Prestamo {
 
+	// Id para la tabla de Prestamo
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="codigo")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "codigo")
 	private int codigo;
-	
-	@Column(name="fechaInicio")
+
+	@Column(name = "fechaInicio")
 	@Temporal(TemporalType.DATE)
 	private Date fechaInicio;
-	
-	@Column(name="fechaFin")
+
+	@Column(name = "fechaFin")
 	@Temporal(TemporalType.DATE)
 	private Date fechaFin;
-	
+
 	@ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="usuario_cedula", nullable=false)
-    private Usuario usuario;
-	
+	@JoinColumn(name = "usuario_cedula", nullable = false)
+	private Usuario usuario;
+
 	@ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="libro_codigo", nullable=false)
-    private Libro libro;
+	@JoinColumn(name = "libro_codigo", nullable = false)
+	private Libro libro;
 
 	public int getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
+
 	public Date getFechaInicio() {
 		return fechaInicio;
 	}
+
 	public void setFechaInicio(Date fechaInicio) {
 		this.fechaInicio = fechaInicio;
 	}
+
 	public Date getFechaFin() {
 		return fechaFin;
 	}
+
 	public void setFechaFin(Date fechaFin) {
 		this.fechaFin = fechaFin;
 	}
+
 	public Usuario getUsuario() {
 		return usuario;
 	}
+
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
+
 	public Libro getLibro() {
 		return libro;
 	}
+
 	public void setLibro(Libro libro) {
 		this.libro = libro;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Prestamo [codigo=" + codigo + ", fechaInicio=" + fechaInicio + ", fechaFin=" + fechaFin + ", usuario="
