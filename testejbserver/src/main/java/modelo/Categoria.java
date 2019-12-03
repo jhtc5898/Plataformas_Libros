a65271d841bd9dcbf5498debbb8c796380ba7b9f
danie <danie@172.16.216.162>
2019-12-03 12:37:30 -0500

    Cambio en la base la columna categoria

--- testejbserver/src/main/java/modelo/Categoria.java
+++ testejbserver/src/main/java/modelo/Categoria.java
@@ -14,44 +14,51 @@ import javax.persistence.OneToMany;
 public class Categoria {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="codigo")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "codigo")
 	private int codigo;
-	
-	@Column(name="nombre")
+
+	@Column(name = "nombre")
 	private String nombre;
-	
-	@Column(name="descripcion")
+
+	@Column(name = "descripcion")
 	private String descripcion;
-	
-	@OneToMany(mappedBy="categoria", cascade = CascadeType.ALL)
-    private List<Libro> libros;
+
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	private List<Libro> libros;
 
 	public int getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
-	}	
+	}
+
 	public List<Libro> getLibros() {
 		return libros;
 	}
+
 	public void setLibros(List<Libro> libros) {
 		this.libros = libros;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Categoria [codigo=" + codigo + ", nombre=" + nombre + ", descripcion=" + descripcion + "]";
